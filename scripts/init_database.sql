91761247ded47022dfb0db7f59e7f42d1358cb49
EdioHub <88396417+EdioHub@users.noreply.github.com>
2026-06-08 21:45:30 -0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,8 +1,6 @@
-
+/*
 =============================================================
-Criar Banco de Dados e Schemas
-
-Para pgad
+Create Database and Schemas
 =============================================================
 Objetivo do Script:
     Este script cria um novo banco de dados chamado 'datawarehouse'.
@@ -15,6 +13,41 @@ AVISO:
     Todos os dados armazenados no banco serão permanentemente excluídos.
     Prossiga com cautela e certifique-se de possuir backups adequados
     antes de executar este script.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+=============================================================
+Criar Banco de Dados e Schemas
+
+Para pgadmin
+=============================================================
+
 =============================================================
 */
 
